6ef0979507ce4df1313be6f415a0fd7a95b791ae
dawguy <wright.david.27@gmail.com>
2026-05-06 09:02:09 -0400

    Phoenix auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :bidirectional_translations, :scopes,
+  user: [
+    default: true,
+    module: BidirectionalTranslations.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: BidirectionalTranslations.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :bidirectional_translations,
   ecto_repos: [BidirectionalTranslations.Repo],
   generators: [timestamp_type: :utc_datetime]
